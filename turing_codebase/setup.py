d7043af895fb082bc3f7e53282b11847940e7d5a
Roozbeh H. Pazuki <rpazuki@gmail.com>
2022-10-05 17:48:07 +0100

    First commit

---
+++ turing_codebase/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+# http://www.diveintopython3.net/packaging.html
+# https://pypi.python.org/pypi?:action=list_classifiers
+
+setup(
+    name="turing",
+    packages=[
+        "solvers.explicit_Euler",
+        "solvers.explicit_Adams_Bashforth",
+        "turing_models",
+    ],
+    include_package_data=True,
+    version="0.0.1",
+    description="Neural networks for Turing pattern inverse problem.",
+    author="Roozbeh H. Pazuki",
+    author_email="r.pazuki@gmail.com",
+    url="https://github.com/rpazuki/TINN",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Science/Research",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
+)
